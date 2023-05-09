6fa5cd676b386fc1c40f4c605417f239a6ae8483
Ayan Alam <ayanalam0025@gmail.com>
2023-05-09 14:40:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/pdfreader/OnPdfSelectListner.java
@@ -0,0 +1,7 @@
+package com.example.pdfreader;
+
+import java.io.File;
+
+public interface OnPdfSelectListner {
+    void onPdfSelected(File file);
+}
---
+++ app/src/main/java/com/example/pdfreader/PdfActivity.java
@@ -0,0 +1,30 @@
+package com.example.pdfreader;
+
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.barteksc.pdfviewer.PDFView;
+
+import java.io.File;
+
+public class PdfActivity extends AppCompatActivity {
+
+    String filepath = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pdf);
+
+
+        PDFView pdfView = findViewById(R.id.pdfView);
+        filepath = getIntent().getStringExtra("path");
+
+        File file = new File(filepath);
+        Uri path = Uri.fromFile(file);
+        pdfView.fromUri(path).load();
+
+    }
+}
